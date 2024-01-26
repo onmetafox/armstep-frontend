4a434a94d324effce47dbb3d4165c5256766c3a6
onmetafox <onemetafox@outlook.com>
2024-01-26 18:01:14 -0500

    fix: team member modal in team page

--- src/views/Team/index.js
+++ src/views/Team/index.js
@@ -15,11 +15,11 @@ import openCV from "../../assets/images/team/OpenCV.svg";
 const teamMembers = [
   {
     name: 'Arman',
-    role: 'Blockchain & NFT Development',
+    role: 'CEO and Founder',
     imgUrl: armanImg,
     upwork: '/upwork',
     linkedin: '/linkedin',
-    about: 'As a full-stack Web3 and Blockchain developer, I have over 6 years of experience in developing end-to-end software solutions. I am proficient in several programming languages including JavaScript, Python, Solidity, and Java. My experience with front-end frameworks includes React,js and I am skilled in back-end development using Node.js, Django, and Flask.',
+    about: "Meet Arman, the CEO and Founder of our esteemed agency, where we turn ambitious ideas into digital realities. With a rich history of partnering with over 50 diverse clients, his journey in the tech industry has been marked by a relentless pursuit of excellence and innovation. Before establishing our agency, he navigated through the tech realm as a Lead Developer and senior executive in more than 10 companies, each with a valuation ranging from $10 million to $100 million. This extensive experience has not only honed my technical expertise in Web, Mobile, Blockchain, and AI Development but also instilled in him a deep understanding of the multifaceted needs of small to mid-sized businesses.",
     stacks: [
       {
         icon: tensorFlow,
@@ -41,11 +41,11 @@ const teamMembers = [
   },
   {
     name: 'Sam',
-    role: 'Blockchain & NFT Development',
+    role: 'CTO and Co-Founder',
     imgUrl: samImg,
     upwork: '/upwork',
     linkedin: '/linkedin',
-    about: 'As a full-stack Web3 and Blockchain developer, I have over 6 years of experience in developing end-to-end software solutions. I am proficient in several programming languages including JavaScript, Python, Solidity, and Java. My experience with front-end frameworks includes React,js and I am skilled in back-end development using Node.js, Django, and Flask.',
+    about: "Meet Samvel, the mastermind behind our cutting-edge technological solutions and the Co-Founder of our agency. With a remarkable track record of working with over 35 clients, Samvel's expertise is the cornerstone of our success in delivering exceptional digital solutions.  As our CTO, Samvel embodies the perfect blend of technical prowess and leadership excellence. He is dedicated to fostering a culture of innovation and precision, ensuring that every project we undertake is a testament to our commitment to quality and ingenuity.",
     stacks: [
       {
         icon: tensorFlow,
@@ -74,11 +74,11 @@ const TeamMemberCard = ({ name, role, imgUrl, upwork, linkedin }) => {
       <h3 className="team-member-name fs-s1">{name}</h3>
       <p className="team-member-role fs-c">{role}</p>
       <div className="social-links">
-        <a href={upwork} target="_blank" rel="noopener noreferrer">
-          <img src={upworkImg} alt="Linkedin" />
-        </a>
         <a href={linkedin} target="_blank" rel="noopener noreferrer">
           <img src={linkedinImg} alt="Linkedin" />
+        </a>
+        <a href={upwork} target="_blank" rel="noopener noreferrer">
+          <img src={upworkImg} alt="Linkedin" />
         </a>
       </div>
     </div>
@@ -100,11 +100,11 @@ const TeamMemberModal = ({ isOpen, onClose, member }) => {
             <h3 className="fs-h4">{member.name}</h3>
             <h4 className="fs-c">{member.role}</h4>
             <div className="social-links">
-              <a href={member.upwork} target="_blank" rel="noopener noreferrer">
-                <img src={upworkImg} alt="Linkedin" />
-              </a>
               <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
                 <img src={linkedinImg} alt="Linkedin" />
+              </a>
+              <a href={member.upwork} target="_blank" rel="noopener noreferrer">
+                <img src={upworkImg} alt="Linkedin" />
               </a>
             </div>
           </div>
