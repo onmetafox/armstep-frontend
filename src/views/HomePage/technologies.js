65d68999e0d9d1752d0a8c881238f996c407fb2a
onmetafox <onemetafox@outlook.com>
2024-01-29 15:50:41 -0800

    feat: finished landing page

--- src/views/HomePage/technologies.js
+++ src/views/HomePage/technologies.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { technologyData } from "src/config/technologyData";
 import TechCard from "src/components/TechCard";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 const Technology = () => {
+    const [active, setActive] = useState(0);
     return <div className="technology-section">
         <div className="container">
             <div className="fs-h2 color-white">Technologies we work with</div>
             <div className="tech-section">
                 <Tabs>
-                    <TabList>
+                    <TabList className="tab-categories">
                     {
                         technologyData.map((item, index) => (
-                            <Tab key={index}>{item.title}</Tab>
+                            <Tab key={index} className={`tab-category fs-s2 color-white m-4 ${active==index?"active": ""}`} onClick={()=>setActive(index)}>{item.title}</Tab>
                         ))
                     }
                     </TabList>
