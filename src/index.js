3bfc8c9ba78829916e1882efaa6cbdec5838216c
onmetafox <onemetafox@outlook.com>
2024-02-23 03:24:33 -0800

    fix: update SEO and uploaded favicon

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider as StoreProvider } from 'react-redux'
-import store from './store';
+// import { Provider as StoreProvider } from 'react-redux'
+// import store from './store';
 
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <StoreProvider store={store}>
+    // <StoreProvider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </StoreProvider>
+    // </StoreProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
